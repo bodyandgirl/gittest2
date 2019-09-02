ba74908fb9d83a6ebb48946dd478056144b47e09
yuhao <test@qq.com>
2019-09-02 17:19:47 +0800

    修改Test.java

--- Text.java
+++ Text.java
@@ -1,1 +1,1 @@
-class Test { public static void main(String[] args){System.out.println("Hello World")}}
+public Class Test { public static void main(String[] args){System.out.println("Hello World")}}
